5e1c3beb196cc02fcf60b207582be2e6ed8e7cff
hupose <amberchenchao@gmail.com>
2013-12-22 22:34:30 +0800

    fix IP has /n problem

--- getIP.php
+++ getIP.php
@@ -7,10 +7,11 @@ require_once("lib/checkBili.class.php");
 
 $ipXml = simplexml_load_file(ROOTPATH . 'conf/ip.xml');
 $lastIP = $ipXml->item[0]->ip;
-
+$lastIP = (string) $lastIP;
+$lastIP = trim($lastIP);
 $getIPUrl = 'http://members.3322.org/dyndns/getip';
 $ip = curlTools::simpleCurl($getIPUrl);
-
+$ip = trim($ip);
 if($lastIP !== $ip){
     $checkBili = new checkBili();
     $checkBili->sendMail("IP地址有变更，新ip地址为：" . $ip);
