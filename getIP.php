82fb8b2575418a720d588fc94ab48821f1c92917
hupose <amberchenchao@gmail.com>
2014-01-04 16:35:38 +0800

    fix bug1

--- getIP.php
+++ getIP.php
@@ -1,13 +1,14 @@
 <?php
+define('FILEPATH',dirname(__FILE__));
 
-require_once("conf/config.php");
-require_once("lib/curlTools.class.php");
-require_once("lib/checkBili.class.php");
-require_once('log4php/Logger.php');
-Logger::configure('log4php/config.xml');
+require_once(FILEPATH . "/conf/config.php");
+require_once(ROOTPATH . "/lib/curlTools.class.php");
+require_once(ROOTPATH . "/lib/checkBili.class.php");
+require_once(ROOTPATH . '/log4php/Logger.php');
+Logger::configure(ROOTPATH . '/log4php/config.xml');
 $log = Logger::getLogger('checkMyPi');
 $log->info("开始检查本机IP");
-$ipXml = simplexml_load_file(ROOTPATH . 'conf/ip.xml');
+$ipXml = simplexml_load_file(ROOTPATH . '/conf/ip.xml');
 $lastIP = $ipXml->item[0]->ip;
 $lastIP = (string) $lastIP;
 $lastIP = trim($lastIP);
@@ -18,7 +19,7 @@ if($lastIP !== $ip){
     $checkBili = new checkBili();
     $checkBili->sendMail("IP地址有变更，新ip地址为：" . $ip);
     $ipXml->item[0]->ip = $ip;
-    $ipXml->saveXML(ROOTPATH . "conf/ip.xml");
+    $ipXml->saveXML(ROOTPATH . "/conf/ip.xml");
     $log->info("IP地址变更啦");
 }
 $log->info("检查IP地址完毕");
